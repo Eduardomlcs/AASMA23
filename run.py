534c3cab1b17a7f7f0f7df118d0133218e11d3c8
Gonçalo Oliveira <goncalo.goulart2001@gmail.com>
2023-06-08 20:20:22 +0100

    changed layout again

---
+++ run.py
@@ -0,0 +1,33 @@
+import argparse
+
+import numpy as np
+from gym import Env
+import random
+
+from taxi import RandomAgent
+from taxi.taxi_env import taxi_env
+
+# create Taxi environment
+env = taxi_env.TaxiEnv(render_mode="human")
+
+# create a new instance of taxi, and get the initial state
+state,info = env.reset()
+num_steps = 99
+taxi = RandomAgent(1)
+taxi2 = RandomAgent(2)
+for s in range(num_steps+1):
+    print(f"step: {s} out of {num_steps}")
+    taxi.see(state)
+    taxi2.see(state)
+    action1 = taxi.action()
+    action2 = taxi2.action()
+    action = (action1,action2)
+    print("Action: ", action)
+    next_state,r,t,_,next_info = env.step(action)
+    # perform this action on the environment
+    env.render()
+    state = next_state
+   
+
+# end this instance of the taxi environment
+env.close()
